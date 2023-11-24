b461eb95ca26c4e05646abc12c39ce7d65261e6e
Grégoire LE CORRE <lecorregregoire@gmail.com>
2023-11-24 20:00:20 +0100

    V1 de l'ast avec la grammaire donnée

--- ast.ml
+++ ast.ml
@@ -1,33 +1,83 @@
-(*Syntaxe abstraite de minipurescript*)
+(* Syntaxe abstraite de minipurescript *)
 
-type ident =
-  | Lident of string
-  | Uident of string
+type lident = string
+type uident = string
 
-type binop =
-  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * / % *)
-  | Beq | Bneq | Blt | Ble | Bgt | Bge  (* == != < <= > >= *)
-  | Band | Bor
+(* Vu que les import sont toujours pareil, il n'y a pas besoin de créer un type import, on voit seulement
+   le type file comme une suite de déclaration. La gestion des imports va être ajoutée directement dans les règles
+   de grammaire *)
 
-type constant =
+
+type file = Fprogramm of decl list (* module Main where { ⟨imports⟩ ⟨decl⟩+; } EOF, donc la liste doit être non
+                                      vide *)
+                                      
+and decl = 
+  | DECLdefn of defn
+  | DECLtdecl of tdecl
+  | DECLdata of uident * (lident list) * uident * (atype list) (* data <uident> <lident>* = (<uident> <atype>* )+ *)
+  | DECLclass of uident * (lident list) * (tdecl list) (* class <uident> <lident>* where { <tdecl>*; } ici les deux listes
+                                                      peuvent être éventuellement vides *)
+  | Dinstance of instance * (tdecl list) (* instance <instance> where { <defn>*; } *)
+
+and defn = DEF of lident * (patarg list) * expr (* <lident> <partag>* = expr *)
+
+and tdecl = TDECL of lident * (lident list) * (ntype list) * (tp list) * tp (* <lident> :: (forall <lident>+ .)?
+                                                                                (<ntype> =>)* (<type> ->)* <type> *)
+
+and ntype = NTP of uident * (atype list) (* <uident> <atype>* *)
+
+and atype = 
+  | ATl of lident
+  | ATu of uident
+  | ATt of tp (* (<type>) *)
+
+and tp = 
+  | TPa of atype
+  | TPn of ntype
+
+and instance = 
+  | INSTntp of ntype
+  | INSTntpc of ntype * ntype (* <ntype> => <ntype> *)
+  | INSTntpcc of (ntype list) * ntype (* ( <ntype>+ ) => <ntype> *)
+
+and patarg = 
+  | PATARconst of constant
+  | PATARlid of lident
+  | PATARuid of uident
+  | PATARpat of pattern (* (<pattern>) *)
+  
+and pattern = 
+  | PATERpatar of patarg
+  | PATERjspquelnom of uident * (patarg list)
+
+and constant =
   | Cbool of bool
   | Cint of int
   | Cstring of string
 
-(*
-
-type atom = 
+and atom = 
   | Aconst of constant
-  | Alident of string
-  | Auident of string
-  | Aexpr of expr
-  | 
+  | Alident of lident
+  | Auident of uident
+  | Aexpr of expr (* Pour les ( <expr> ) *)
+  | Aexprtype of expr * tp
 
-*)
-
-
-type expr = 
+and expr = 
   | Econstant of constant
-  | Eident of ident
-  | Ebinop of binop * expr * expr
-  
+  | Eident of lident
+  | Ebinop of binop * expr * expr (* On rajoute pas l'opérateur unaire -, on le verra comme -v = Ebinop(Bsub, 0, v) *)
+  | Efonct of lident * (atom list)
+  | Emodule of uident * (atom list) (* Dans les deux cas la liste ne peux pas être vide *)
+  | Econd of expr * expr * expr (* if e1 then e2 else e3 *)
+  | Edo of (expr list) (* do { <expr>+ ;} *)
+  | Eaffect of (binding list) * expr (* let { <bindings>+ } in <expr> *)
+  | Ecase of expr * (branch list) (* case <expr> of { <branch>+; } *)
+
+and binding = Baffect of lident * expr (* <lident> = <expr> *)
+
+and branch = Barrow of pattern * expr (* <pattern> -> <expr>  *)
+
+and binop =
+  | Badd | Bsub | Bmul | Bdiv | Bmod    (* + - * / % *)
+  | Beq | Bneq | Blt | Ble | Bgt | Bge  (* == != < <= > >= *)
+  | Band | Bor
