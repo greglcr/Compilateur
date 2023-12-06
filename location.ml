66245379d027dcb2122ca06b65069844553dcaa2
Hubert Gruniaux <hubert@gruniaux.com>
2023-12-06 10:58:08 +0100

    Revert creation of location for now

--- location.ml
+++
@@ -1,8 +0,0 @@
-type t = {
-    (* The source file name. *)
-    file : string;
-    (* The line number. The first line is numbered 1. *)
-    lineno : int;
-    (* The column number. The first column is numbered 1. *)
-    colno : int;
-}
