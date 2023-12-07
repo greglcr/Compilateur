6f863875e37196101e7cf16865d5edb165c9a109
Grégoire LE CORRE <lecorregregoire@gmail.com>
2023-12-07 16:22:53 +0100

    Add unify in typer

--- typer.ml
+++ typer.ml
@@ -6,6 +6,13 @@ exception Error of string
 module SMap = Map.Make(String)
 module SSet = Set.Make(String)
 
+module V = struct
+    type t = tvar
+    let compare v1 v2 = Stdlib.compare v1.id v2.id
+    let equal v1 v2 = v1.id = v2.id
+    let create = let r = ref 0 in fun () -> incr r; { id = !r; def = None }
+end
+
 type env =
     {
         decls : (string, Ast.decl) Hashtbl.t
@@ -123,45 +130,89 @@ let rec expr state = function
     
     | _ -> raise (Error "not yet implemented")
 
-let rec find_functions functions decls =
-    let handle_decl decl = match decl with
-        | Pdecl_func (name, patterns, expr) -> (
-            Hashtbl.add functions name.node decl
-        )
-        | Pdecl_func_signature _ -> ()
-        | _ -> ()
-    in match decls with
-    | [] -> functions
-    | decl::remaining -> (handle_decl decl.node); find_functions  functions remaining
+    let rec find_functions functions decls =
+        let handle_decl decl = match decl with
+            | Pdecl_func (name, patterns, expr) -> (
+                Hashtbl.add functions name.node decl
+            )
+            | Pdecl_func_signature _ -> ()
+            | _ -> ()
+        in match decls with
+        | [] -> functions
+        | decl::remaining -> (handle_decl decl.node); find_functions  functions remaining
+    
+    let type_func functions name =
+        let all_declarations = Hashtbl.find_all functions name in
+        (*
+           We use the following algorithm to simplify functions declarations :
+           - If there is a single declaration for the function => We do nothing.
+           - If not, we compress all the different declarations in a single function
+             declaration using the case expression. For example, if we have :
+                f a = e1
+                f b = e2
+                f c = e3
+    
+             Then we "compress" it to :
+                f e =
+                    case e of
+                    | a -> e1
+                    | b -> e2
+                    | c -> e3
+    
+             However, if two declarations does not have the same amount of patterns
+             or if at least one declaration have more than 1 pattern then we
+             emit an error. For example :
+                f a b = e1
+                f c d = e2
+                -- ERROR: we can not compress the two declarations into one
+        *)
+        ()
+    
+    let file = function
+        | Ast.Fprogram decls -> 
+            let functions = find_functions (Hashtbl.create 31) decls in
+            let functions_name = SSet.of_seq (Hashtbl.to_seq_keys functions) in
+            SSet.iter (fun name -> type_func functions name) functions_name
 
-let type_func functions name =
-    let all_declarations = Hashtbl.find_all functions name in
-    (*
-       We use the following algorithm to simplify functions declarations :
-       - If there is a single declaration for the function => We do nothing.
-       - If not, we compress all the different declarations in a single function
-         declaration using the case expression. For example, if we have :
-            f a = e1
-            f b = e2
-            f c = e3
+let rec head = function
+    | Ttyp_var { def = Some t } -> head t
+    | t -> t
 
-         Then we "compress" it to :
-            f e =
-                case e of
-                | a -> e1
-                | b -> e2
-                | c -> e3
+(* 
+let rec canon t = match head t with
+    | Ttyp_unit | Ttyp_boolean | Ttyp_int | Ttyp_string as t -> t
+    | Ttyp_effect t -> Ttyp_effect (canon t)
+    | Ttyp_var tv -> Ttyp_var ({ id = tv.id; def = canon (head tv.def) })
+*)
 
-         However, if two declarations does not have the same amount of patterns
-         or if at least one declaration have more than 1 pattern then we
-         emit an error. For example :
-            f a b = e1
-            f c d = e2
-            -- ERROR: we can not compress the two declarations into one
-    *)
 
-let file = function
-    | Ast.Fprogram decls -> 
-        let functions = find_functions (Hashtbl.create 31) decls in
-        let functions_name = SSet.of_seq (Hashtbl.to_seq_keys functions) in
-        SSet.iter (fun name -> type_func functions name) functions_name
+let rec occur v t = match head t with
+    | Ttyp_var w -> V.equal v w
+    | _ -> false
+
+
+exception UnificationFailure of Typedtree.typ * Typedtree.typ
+let unification_error t1 t2 = raise (UnificationFailure (t1, t2))
+
+let rec unify t1 t2 = match head t1, head t2 with
+    | Ttyp_int, Ttyp_int 
+    | Ttyp_boolean, Ttyp_boolean 
+    | Ttyp_string, Ttyp_string
+    | Ttyp_unit, Ttyp_unit -> ()
+    | Ttyp_var v1, Ttyp_var v2 when V.equal v1 v2 -> ()
+    | (Ttyp_var v1 as t1), t2 -> if occur v1 t2 then unification_error t1 t2;
+                                 assert (v1.def = None);
+                                v1.def <- Some t2
+    | t1, (Ttyp_var v2 as t2) -> unify t2 t1
+    | Ttyp_function (pl1, e1), Ttyp_function (pl2, e2) -> unify_params pl1 pl2;
+                                                                  unify e1 e2
+    | _ -> ()
+
+and unify_params p1 p2 = match (p1, p2) with
+    | [], [] -> ()
+    | t1::r1, t2::r2 -> unify t1 t2; unify_params r1 r2
+    | [], _ 
+    | _, [] -> raise (Error "not the same number of parameters")
+         
+
+
